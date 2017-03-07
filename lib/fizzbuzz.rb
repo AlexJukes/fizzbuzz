d2e6ab94f26e3733ab273132139e6a7c9a3997ea
Alex <alexanderjukes@gmail.com>
2017-03-07 14:46:14 +0000

    Passed buzz when 5, fizzbuzz when 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,6 +2,10 @@ def fizzbuzz(number)
   # only return fizz if num = 3
   if number == 3
     'fizz'
+  elsif number == 5
+    'buzz'
+  elsif number == 15
+    'fizzbuzz'
   else number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,10 @@ describe 'fizzbuzz' do
   it 'returns num when passed num that is not 3' do # Given number that is not 3
     expect(fizzbuzz(4)).to eq 4
   end
+  it 'returns "buzz" when num is 5' do # Given 5
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when num is 15' do #Given 15
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
